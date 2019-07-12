86763011be1ee929ac332069fef6c7bcd8fc06b4
qin-ziqi <348633040@qq.com>
2019-07-12 15:03:39 +0800

    dev-server-plugin

--- webpack-vue/src/index.js
+++ webpack-vue/src/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './app.vue';
 
+import './assets/style/common.css';
+import './assets/style/common.styl';
+import './assets/images/tim1.gif';
+
 const root = document.createElement('div');
 document.body.appendChild(root);
 
